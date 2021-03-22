c8ec4fc575c7567ca33f674c3b7779ab48fab8b7
Joe Riley <joeyriley@gmail.com>
2021-03-21 19:16:48 -0700

    Get delete endpoint working

--- routes/apiRoutes/routes.js
+++ routes/apiRoutes/routes.js
@@ -9,7 +9,6 @@ router.get('/notes', (req, res) => {
 
 router.post('/notes', (req, res) => {
   const db = JSON.parse(fs.readFileSync('./db/db.json', 'UTF-8'));
-  console.log(req.body);
   const note = req.body;
   note.id = uuidV4();
 
@@ -19,5 +18,22 @@ router.post('/notes', (req, res) => {
 
   res.json(note);
 });
+
+router.delete('/notes/:id', (req, res) => {
+  const db = JSON.parse(fs.readFileSync('./db/db.json', 'UTF-8'));
+  console.log(req.params)
+  const newDb = []
+  db.forEach(note => {
+    if (note.id != req.params.id) {
+      newDb.push(note);
+    }
+  })
+
+  fs.writeFileSync('./db/db.json', JSON.stringify(newDb));
+
+  res.json({message: `Note with id ${req.params.id} deleted.`});
+
+})
+
 
 module.exports = router;
