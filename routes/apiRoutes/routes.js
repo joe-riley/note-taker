57df361e1fc497f01aca654aa780f471bcee4074
Joe Riley <joeyriley@gmail.com>
2021-03-21 18:00:01 -0700

    Get get and post notes working

--- routes/apiRoutes/routes.js
+++ routes/apiRoutes/routes.js
@@ -3,9 +3,21 @@ const fs = require("fs");
 const express = require("express");
 const { v4: uuidV4 } = require("uuid");
 
-router.get("/notes", (req, res) => {
-  res.json({hi: "joe"});
-})
+router.get('/notes', (req, res) => {
+  res.json(JSON.parse(fs.readFileSync('./db/db.json', 'UTF-8')));
+});
 
+router.post('/notes', (req, res) => {
+  const db = JSON.parse(fs.readFileSync('./db/db.json', 'UTF-8'));
+  console.log(req.body);
+  const note = req.body;
+  note.id = uuidV4();
+
+  db.push(note);
+
+  fs.writeFileSync('./db/db.json', JSON.stringify(db));
+
+  res.json(note);
+});
 
 module.exports = router;
