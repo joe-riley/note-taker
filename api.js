b844d9dccdc53b9a2136b806b780fe1ee44bcb8e
Joe Riley <joeyriley@gmail.com>
2021-03-21 19:24:58 -0700

    Let Heroku use its own port

--- api.js
+++ api.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true}));
 app.use('/', htmlRoutes);
 app.use('/api', apiRoutes);
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Api is now listening on port ${PORT}`);
 });
